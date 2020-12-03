861d805e628f92e3ab63ef2dfbab961ab9b7e44f
LucaBallistri <ballistri.luca@gmail.com>
2020-12-03 21:50:53 +0100

    fixed some problems with parsing

--- database.py
+++ database.py
@@ -5,13 +5,12 @@ import random
 import os
 
 # connect sql file with python file
-conn = None  
-
+conn = None
 # execute the sql file in python
 cursor = None
 
 def open_and_create(path):
-	global conn 
+	global conn
 	global cursor
 
 	conn = sqlite3.connect(path)
@@ -23,15 +22,14 @@ def open_and_create(path):
 
 	except sqlite3.OperationalError: 
 		# if table does not exist, we create one
-		create_user_table(conn, cursor)
+		create_user_table()
 
 def create_user_table():
 	global conn 
 	global cursor
 
 	# create table
-	cursor.execute('''CREATE TABLE user
-		(username TEXT, password TEXT, salt INT, PRIMARY KEY(username))''')
+	cursor.execute('''CREATE TABLE user(username TEXT, password TEXT, salt INT, PRIMARY KEY(username))''')
 
 # insert data inside the table
 def save_new_username(username, password):
@@ -46,8 +44,7 @@ def save_new_username(username, password):
 	digest = hashlib.sha256(password.encode("utf-8")).hexdigest()
 
 	# prepared statement to avoid sql injection
-	cursor.execute("INSERT OR REPLACE INTO user (?,?,?)", 
-		(username, digest, salt))
+	cursor.execute("INSERT OR REPLACE INTO user VALUES (?,?,?)", (username, digest, salt))
 
 	conn.commit()
 
@@ -56,7 +53,7 @@ def check_for_username(username, password):
 	global cursor
 
 	# prepare statement to avoid sql injection
-	rows = cursor.execute("SELECT * FROM user WHERE username = ?", (username,))
+	rows = cursor.execute("SELECT * FROM user") # WHERE username = ? AND password = ?", (username, password))
 	conn.commit()
 	results = rows.fetchall()
 
@@ -68,53 +65,33 @@ def check_for_username(username, password):
 	# check if computed digest is equal to stored digest
 	if digest == results[0][1].lower():
 		return True 
-
 	else: 
 		return False
 
-def parse_args():
+def parse_argument():
 	parser = argparse.ArgumentParser()
-
+	
 	# command to add a user
-	parser.add_argument("-a", help = "add a username (requires -p)",
-		required = False)
-
-	# command for the user password
-	parser.add_argument("-p", help = "the username password ",
-		required = True)
+	parser.add_argument("-a", help = "add a username (requires -p)", required = False, action = "store_true", default = None)
 
 	# command to check user credentials
-	parser.add_argument("-c", help = "check for a username and a password, (requires -p",
-		required = False)
+	parser.add_argument("-u", help = "check for a username and a password, (requires -p)", required = False, default = None)
+
+	# command for the user password
+	parser.add_argument("-p", help = "the username password ", required = True, default = None)
 
 	return parser.parse_args()
 
+if __name__ == "__main__":
+	
+	path = os.path.abspath(os.path.join(os.getcwd(), 'covid_package/database/database.db'))
 
-args = parse_args()
+	open_and_create(path)
 
-path = os.path.abspath(os.path.join(os.getcwd(), 'covid_package/data/database.db'))
+	args = parse_argument()
 
-open_and_create(path)	
-
-if args.a and args.p: 
-	save_new_username(args.a, args.p)
-elif args.c and args.p: 
-	check_for_username(args.c, args.p)
-conn.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	if args.a and args.p: 
+		save_new_username(args.a, args.p)
+	elif args.u and args.p: 
+		check_for_username(args.u, args.p)
+	conn.close()
