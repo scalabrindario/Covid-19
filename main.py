f6d9518de04efa841b745f3f471011753fe14c8d
Pietro Querci <pietroquerci@MBP-di-Pietro-2.homenet.telecomitalia.it>
2020-12-04 21:52:59 +0100

    fixed some functions for the test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from covid_package.script.covid19 import live_cases_world
 from database import open_and_create, check_for_username
+from covid_package.script.csv_maker import country_check, country_name
 import argparse
 import pandas as pd
 
@@ -7,14 +8,10 @@ import pandas as pd
 default_csv_path = "covid_package/csv/"
 default_db_path = "covid_package/database/"
 
-# Storing information abount countries
-country_df = pd.read_csv(default_csv_path + "country_list.csv", sep = ";")
-country_df = country_df.set_index("ISO2")
-
 def parse_argument():
 	parser = argparse.ArgumentParser()
 	# Selection of the country through its country code that can be found in covid_package/csv/country_list.csv
-	parser.add_argument("country_code", help = "The country code associated with each country", choices = list(country_df.index))
+	parser.add_argument("country_code", help = "The country code associated with each country")
 	# Selectio of the level of verbosity
 	parser.add_argument("-v", help = "Be more verbose", action = "store_true")
 	# Selection of the timespan for the data shown
@@ -28,34 +25,38 @@ def parse_argument():
 
 country = parse_argument()
 
-if __name__ == "__main__":
-	# Open database and if necessary create the user table
-	open_and_create(default_db_path + "database.db")
+if country.country_code in country_check(default_csv_path + "country_list.csv"):
+	if __name__ == "__main__":
+		# Open database and if necessary create the user table
+		open_and_create(default_db_path + "database.db")
 
-	if check_for_username(country.u, country.p):
-		df = live_cases_world()
-		# Name of the selected country code
-		country_name = country_df.loc[country.country_code]["Country"]
-		# Date of the last update
-		date = df.loc[country.country_code]["Date"][:10]
-		# Most recent confirmed cases
-		new_cases = df.loc[country.country_code]["NewConfirmed"]
-		# Most recent confirmed deaths
-		new_deaths = df.loc[country.country_code]["NewDeaths"]
-		# Most recent confirmed recoveries
-		new_recovery = df.loc[country.country_code]["NewRecovered"]
-		# Overall confirmed cases
-		total_cases = df.loc[country.country_code]["TotalConfirmed"]
-		# Overall confirmed deaths
-		total_deaths = df.loc[country.country_code]["TotalDeaths"]
-		# Overall confirmed recoveries
-		total_recovery = df.loc[country.country_code]["TotalRecovered"]
+		if check_for_username(country.u, country.p):
+			df = live_cases_world()
+			# Name of the selected country code
+			country_name = country_name(default_csv_path + "country_list.csv", country.country_code)
+			# Date of the last update
+			date = df.loc[country.country_code]["Date"][:10]
+			# Most recent confirmed cases
+			new_cases = df.loc[country.country_code]["NewConfirmed"]
+			# Most recent confirmed deaths
+			new_deaths = df.loc[country.country_code]["NewDeaths"]
+			# Most recent confirmed recoveries
+			new_recovery = df.loc[country.country_code]["NewRecovered"]
+			# Overall confirmed cases
+			total_cases = df.loc[country.country_code]["TotalConfirmed"]
+			# Overall confirmed deaths
+			total_deaths = df.loc[country.country_code]["TotalDeaths"]
+			# Overall confirmed recoveries
+			total_recovery = df.loc[country.country_code]["TotalRecovered"]
 
-		# Definition of the behaviour based on timespan selection
-		if country.t == "partial": 
-			print("{} live update as of {}: \n - New Confirmed Cases: {} \n - New Deaths Cases: {} \n - New Recoveries: {} ".format(country_name, date, new_cases, new_deaths, new_recovery ))
-		elif country.t == "total":
-			print("{} total cases since the breakout as of {}: \n - Total Confirmed Cases: {} \n - Total Deaths Cases: {} \n - Total Recoveries: {} ".format(country_name, date, total_cases, total_deaths, total_recovery)) 
+			# Definition of the behaviour based on timespan selection
+			if country.t == "partial": 
+				print("{} live update as of {}: \n - New Confirmed Cases: {} \n - New Deaths Cases: {} \n - New Recoveries: {} ".format(country_name, date, new_cases, new_deaths, new_recovery ))
+			elif country.t == "total":
+				print("{} total cases since the breakout as of {}: \n - Total Confirmed Cases: {} \n - Total Deaths Cases: {} \n - Total Recoveries: {} ".format(country_name, date, total_cases, total_deaths, total_recovery)) 
 
-	else:
-		print ('Username does not exist or password is incorrect')
+		else:
+			print ('Username does not exist or password is incorrect')
+else:
+	print ('Please type a correct country code')
+
