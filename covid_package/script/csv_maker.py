ed0ef7588ef13b1de1d760fdc1fa715133289d59
Pietro Querci <pietroquerci@MBP-di-Pietro-2.homenet.telecomitalia.it>
2020-12-04 21:55:21 +0100

    added functions for the test

--- covid_package/script/csv_maker.py
+++ covid_package/script/csv_maker.py
@@ -12,5 +12,34 @@ def get_country_code(URL = "https://api.covid19api.com/countries"):
     
     return df
 
+def country_check(path):
+	try:
+		# Storing information abount countries
+		country_df = pd.read_csv(path, sep = ";")
+		country_df = country_df.set_index("ISO2")
+		return list(country_df.index)
+
+	except FileNotFoundError:
+	    print("The file does not exist")
+	    return
+
+	except ValueError:
+		print("File has a wrong encoding")
+		return
+
+	except UnicodeDecodeError:
+		print("File has a wrong encoding")
+		return
+	
+
+def country_name(path, code):
+	# Storing information abount countries
+	country_df = pd.read_csv(path, sep = ";")
+	country_df = country_df.set_index("ISO2")
+
+	return country_df.loc[code]["Country"]
+
+	
+
 df = get_country_code()
-df.to_csv("../csv/country_list.csv", sep = ";")
+df.to_csv("covid_package/csv/country_list.csv", sep = ";")
